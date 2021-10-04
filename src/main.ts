8c6a3d704dc61d036a88bc49a8cf7337fa3600f1
Nikoloz Bokeria <nikolozbokeria19@gmail.com>
2021-10-04 21:18:13 +0400

    feat: add whitelist of origins

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,22 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const whitelist = configService.get('FRONTEND_URL').split(',');
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.enableCors({
-    origin: configService.get('FRONTEND_URL'),
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error(`${origin} Not allowed by CORS, allowed ${whitelist.join(',')}`))
+      }
+    },
     credentials: true,
     exposedHeaders: 'Authentication',
   });
